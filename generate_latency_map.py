09908f90d349e5fc3fe16b4dd36b1021b315a9ee
datar <tianxian@gmail.com>
2016-06-29 17:56:34 -0700

    add new files

--- generate_latency_map.py
+++ generate_latency_map.py
@@ -2,6 +2,10 @@ import sys
 import csv
 
 LATENCY_DATA_HEADER = 'tx,inner,net'
+INNER_HEADER = 'latency_in'
+NET_HEADER = 'latency_net'
+COUNT_HEADER = 'count'
+
 INNER_LATENCY_MAP_HEADER = 'latency,count'
 NETWORK_LATENCY_MAP_HEADER = 'latency,count'
 USAGE = 'python generate_latency_map.py data_in_file inner_latency_out_file network_latency_out_file'
@@ -19,10 +23,15 @@ def transfer_latency_to_map(source, inner_latency_file, network_latency_file):
 
     with open(inner_latency_file, 'wb') as outfile_in_lat, open(network_latency_file,'wb') as outfile_net_lat:
         data_in_writer = csv.writer(outfile_in_lat, quoting=csv.QUOTE_NONE)
-        data_net_writer = csv.writer(outfile_net_lat, quoting=csv.QUOTE_NONE)
-        for k in sorted(in_lat_map.keys()):
+        data_in_writer.writerow([INNER_HEADER, COUNT_HEADER])
+        in_lat_map.pop(INNER_HEADER, None)
+        for k in in_lat_map.keys():
             data_in_writer.writerow([k, in_lat_map[k]])
-        for k in sorted(net_lat_map.keys()):
+
+        data_net_writer = csv.writer(outfile_net_lat, quoting=csv.QUOTE_NONE)
+        data_net_writer.writerow([NET_HEADER, COUNT_HEADER])
+        net_lat_map.pop(NET_HEADER, None)
+        for k in net_lat_map.keys():
             data_net_writer.writerow([k, net_lat_map[k]])
     return 0
 
